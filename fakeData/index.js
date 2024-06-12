21a580cbb9d2f7e3fb08ba9a0a4be412d7254ccd
thinhnh254 <nguyenhungthinh2504@gmail.com>
2024-06-12 11:24:20 +0700

    how to resolver work

---
+++ fakeData/index.js
@@ -0,0 +1,32 @@
+export default {
+  authors: [
+    {
+      id: "1",
+      name: "Tom Nguyen",
+    },
+    {
+      id: "11",
+      name: "Thinh Nguyen",
+    },
+  ],
+  folders: [
+    {
+      id: "1",
+      name: "Folder 1",
+      createdAt: "2021-09-01",
+      authorID: "1",
+    },
+    {
+      id: "2",
+      name: "Folder 2",
+      createdAt: "2021-09-09",
+      authorID: "11",
+    },
+    {
+      id: "3",
+      name: "Folder 3",
+      createdAt: "2021-08-01",
+      authorID: "1",
+    },
+  ],
+};
--- server.mjs
+++ server.mjs
@@ -5,22 +5,40 @@ import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHt
 import bodyParser from "body-parser";
 import { expressMiddleware } from "@apollo/server/express4";
 import cors from "cors";
+import fakeData from "./fakeData/index.js";
 
 const app = express();
 const httpServer = http.createServer(app);
 
 const typeDefs = `#graphql
-  type Query {
-    name: String
+  type Folder {
+    id: String,
+    name: String,
+    createdAt: String,
+    authors : Author
   }
 
- 
+  type Author{
+    id: String,
+    name: String,
+  }
+
+  type Query {
+    folders: [Folder]
+  }
 `;
 
 const resolvers = {
   Query: {
-    name: () => {
-      return "Tom Nguyen";
+    folders: () => {
+      return fakeData.folders;
+    },
+  },
+  Folder: {
+    authors: (parent, args) => {
+      console.log({ parent, args });
+      const authorId = parent.authorID;
+      return fakeData.authors.find((author) => author.id === authorId);
     },
   },
 };
